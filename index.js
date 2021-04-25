f560ff92a8da50f915e7f44493c531cd41978f2b
Michael Martinez <zeroeclipse@live.com>
2021-04-25 13:28:00 -0700

    commit tree c2103f8ca080f7bace63cfc48630d519ad782aff

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+let main = document.getElementById("main");
+main.remove()
+
+let newHeader = document.createElement('h1')
+newHeader.id = "victory"
+newHeader.innerHTML = "YOUR NAME is the champion!";
+newHeader.className = "victory";
